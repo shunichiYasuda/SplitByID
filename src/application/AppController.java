2160ae8ec7e23bf9a962bea275c3aac91f2fab25
yasuda <yasuda@192.168.3.11>
2018-02-12 13:21:46 +0900

    new Project on eclipse

--- src/application/AppController.java
+++ src/application/AppController.java
@@ -1,5 +1,33 @@
 package application;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AppController {
-	
+	File baseFile,selectedRcordFile,nonSelectedRecordFile,refferIDFile;
+	List<String> baseRecordList = new ArrayList<String>();
+	List<String> selectedRecordList = new ArrayList<String>();
+	List<String> nonSelectedRecordList = new ArrayList<String>();
+	List<String> refferIDList = new ArrayList<String>();
+	@FXML
+	TextField suffixName;
+	@FXML
+	TextArea log;
+	@FXML
+	private void quitAction() {
+		System.exit(0);
+	}
+	@FXML
+	private void openAction() {
+		
+	}
+	@FXML
+	private void execAction() {
+		
+	}
 }
